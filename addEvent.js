690d7bee16948ecf3c33f1d126d51cb487bde702
kuntang <kuntang@163.com>
2017-12-21 16:43:11 +0800

    run build

--- addEvent.js
+++ addEvent.js
@@ -1,20 +1,17 @@
-/**
- * @desc 为对象添加事件对象
- * @param {Object} obj
- * @param {String} event
- * @param {Function} fn
- * @return {Function|Boolean}
- */
-function addEvent(obj, event, fn) {
-  if (obj.attachEvent) {
-    obj.attachEvent('on' + event, fn);
-  } else if (obj.addEventListener) {
-    obj.addEventListener(event, fn, false);
+const addEvent = (function() {
+  if (window.addEventListener) {
+    return function(obj, evt, fn, useCapture) {
+      return obj.addEventListener(evt, fn, useCapture)
+    }
+  } else if (window.attachEvent) {
+    return function(obj, evt, fn) {
+      return obj.attachEvent('on' + evt, fn)
+    }
   } else {
-    console.error('add event failed!!');
-    return false;
+    return function(obj, evt, fn) {
+      obj['on' + evt] = fn
+    }
   }
-  return fn;
-}
+})();
 
 modules.export = addEvent;
